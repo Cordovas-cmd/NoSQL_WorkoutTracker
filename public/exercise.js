8a70a1a78244682d77bf5890f8676ec4e4ed92cc
Samuel Cordova <cordovas1337@gmail.com>
2021-05-24 22:08:57 -0500

    added some eventhandlers for data submission

--- public/exercise.js
+++ public/exercise.js
@@ -98,3 +98,24 @@ function validateInputs() {
     addButton.setAttribute("disabled", true);
   }
 }
+
+//event handler for form submission data entry.
+async function handleFormSubmit(event) {
+    event.preventDefault();
+  
+    let workoutData = {};
+  
+    if (workoutType === "cardio") {
+      workoutData.type = "cardio";
+      workoutData.name = cardioNameInput.value.trim();
+      workoutData.distance = Number(distanceInput.value.trim());
+      workoutData.duration = Number(durationInput.value.trim());
+    } else if (workoutType === "resistance") {
+      workoutData.type = "resistance";
+      workoutData.name = nameInput.value.trim();
+      workoutData.weight = Number(weightInput.value.trim());
+      workoutData.sets = Number(setsInput.value.trim());
+      workoutData.reps = Number(repsInput.value.trim());
+      workoutData.duration = Number(resistanceDurationInput.value.trim());
+    }
+  
