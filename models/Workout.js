f7ea6cfb8460252a3de59bb37e63a4097efc9f88
Samuel Cordova <cordovas1337@gmail.com>
2021-05-24 21:54:22 -0500

    added the api.js code and think I added everything I need for the package dependancies

--- models/Workout.js
+++ models/Workout.js
@@ -1,61 +1,61 @@
-// // Dependencies
-// // =============================================================
-// const mongoose = require("mongoose");
+// Dependencies
+// =============================================================
+const mongoose = require("mongoose");
 
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// // Model
-// // =============================================================
-// const workoutSchema = new Schema({
-//   day: {
-//     type: Date,
-//     default: Date.now
-//   },
-//   exercises: [
-//     {
-//       type: {
-//         type: String,
-//         required: "Please enter the workout type."
-//       },
-//       name: {
-//         type: String,
-//         required: "Please enter the workout name."
-//       },
-//       duration: {
-//         type: Number,
-//         required: "Please enter the duration."
-//       },
-//       weight: {
-//         type: Number,
+// Model
+// =============================================================
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Please enter the workout type."
+      },
+      name: {
+        type: String,
+        required: "Please enter the workout name."
+      },
+      duration: {
+        type: Number,
+        required: "Please enter the duration."
+      },
+      weight: {
+        type: Number,
 
-//       },
-//       reps: {
-//         type: Number,
-//       },
-//       sets: {
-//         type: Number,
-//       },
-//       distance: {
-//         type: Number,
-//       }
-//     }
-//   ]
-// },
-//   {
-//     toJSON: {
-//       virtuals: true,
-//     },
-//   }
-// );
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      }
+    }
+  ]
+},
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
-// workoutSchema.virtual("totalDuration").get(function () {
-//   return this.exercises.reduce((total, exercise) => {
-//     return total + exercise.duration;
-//   }, 0);
-// });
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
 
-// const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
-// // Export
-// // =============================================================
-// module.exports = Workout;
+// Export
+// =============================================================
+module.exports = Workout;
