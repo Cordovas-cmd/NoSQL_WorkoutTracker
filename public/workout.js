a7e751eac1cd34bd1422210b2467c5c4503b41bb
Samuel Cordova <cordovas1337@gmail.com>
2021-05-24 22:33:11 -0500

    finished adding the workout js with soem comments, will add more details later

--- public/workout.js
+++ public/workout.js
@@ -50,4 +50,41 @@ async function initWorkout() {
   //tell where to show bupdated data from html selection.
   function renderWorkoutSummary(summary) {
     const container = document.querySelector(".workout-stats");
-  
+    
+    // object with properties tied to total workout stats.
+    const workoutKeyMap = {
+        date: "Date",
+        totalDuration: "Total Workout Duration",
+        numExercises: "Exercises Performed",
+        totalWeight: "Total Weight Lifted",
+        totalSets: "Total Sets Performed",
+        totalReps: "Total Reps Performed",
+        totalDistance: "Total Distance Covered"
+      };
+    // ties the object and summary together.
+      Object.keys(summary).forEach(key => {
+          // for each 
+        const p = document.createElement("p");
+        const strong = document.createElement("strong");
+    
+        strong.textContent = workoutKeyMap[key];
+        const textNode = document.createTextNode(`: ${summary[key]}`);
+    
+        p.appendChild(strong);
+        p.appendChild(textNode);
+    
+        container.appendChild(p);
+      });
+    }
+    
+    function renderNoWorkoutText() {
+      const container = document.querySelector(".workout-stats");
+      const p = document.createElement("p");
+      const strong = document.createElement("strong");
+      strong.textContent = "You have not created a workout yet!"
+    
+      p.appendChild(strong);
+      container.appendChild(p);
+    }
+    
+    initWorkout();
