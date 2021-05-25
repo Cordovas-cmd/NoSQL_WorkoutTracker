f0b649d4a694c21b4ebdc5649722d6dbad9189ac
Samuel Cordova <cordovas1337@gmail.com>
2021-05-24 22:51:10 -0500

    added server.js and seeded data

---
+++ server.js
@@ -0,0 +1,33 @@
+
+// Dependencies
+// =============================================================
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+// App Variables
+// =============================================================
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// App config
+// =============================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.use(require('./routes'));
+
+// Start
+// =============================================================
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
