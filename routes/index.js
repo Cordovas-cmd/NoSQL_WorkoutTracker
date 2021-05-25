3cf9cec172e94b37d6c8bda638999373c027563f
Samuel Cordova <cordovas1337@gmail.com>
2021-05-24 22:38:25 -0500

    added the index.js in routes to tie the other files together and export one ref

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,13 @@
+// Dependencies
+const router = require('express').Router();
 
+// ties both api and homeRoutes.js together for later ref
+const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api');
+
+// Routes
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+// Export
+module.exports = router;
